7b3fa494a90bf764aa5a9846011d5490fb4607c2
Robert Neville <robertneville27@gmail.com>
2022-09-15 14:19:23 +1000

    Change app bg - it was too dark

--- src/themes/default.ts
+++ src/themes/default.ts
@@ -2,7 +2,7 @@ import Theme from "./Theme";
 
 const defaultTheme = {
   app: {
-    bg: "#B2BABB",
+    bg: "#E5E8E8",
   },
   menuBar: {
     bg: "#17202A",
@@ -16,7 +16,7 @@ const defaultTheme = {
   },
   splashScreen: {
     fg: "#212F3D",
-    bg: "#B2BABB",
+    bg: "#E5E8E8",
   },
   editor: {
     bg: "#212F3D",
